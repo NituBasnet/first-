2f71f22aa1642dcb1c1251ec1531fe008f82e814
Nitu Basnet <nitubasnet@ims.edu.np>
2023-06-17 21:50:15 +0545

    second

---
+++ even.js
@@ -0,0 +1,5 @@
+//even number using for loop
+console.log('for loop');
+for(i=0; i<=10; i+2){
+    console.log(i);
+}
---
+++ nitutest2.js
@@ -0,0 +1,5 @@
+//i am going to print  1 to 10 using for loop because it is my hw 
+console.log('for loop');
+for(let i=1; i<=10; i++){
+    console.log(i)
+}
